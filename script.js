56291edc42f3f62e268b059943ea0ba9b4543027
oindridebmallick <oindri.debmallick@gmail.com>
2025-02-28 13:45:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,22 @@ function getBotResponse(input) {
     let responses = {
         "hello": "Hi! How can I help you today?",
         "what is a loan?": "A loan is a sum of money borrowed that must be repaid with interest.",
-        "how to save money?": "Create a budget, track expenses, and cut unnecessary costs."
+        "how to save money?": "Create a budget, track expenses, and cut unnecessary costs.",
+        "How can I save money if I don’t earn much?" : "Keep a little aside every time you get paid.",
+        "Where can I keep my money safe?" : "A bank account or a digital wallet is safer than cash.",
+        "How do I open a bank account?" : "Visit a bank with an ID and a small deposit.",
+        "Do I need to know reading to use a bank?" : "No, bank staff can help you",
+        "Can I save money without a bank?" : "Yes, in trusted savings groups or self-help groups.",
+        "How can I get a small loan?" : "Apply at microfinance banks or women’s self-help groups.",
+        "Do I need papers to get a loan?" : " Some places ask for ID, but small loans may not.",
+        "Can I borrow money without a job?" : "Some loans are for business ideas, not jobs.",
+        "How do I return a loan?" : "Pay small amounts every month.",
+        "What happens if I can’t pay my loan?" : "Talk to the lender and ask for more time.",
+        "How can I earn money from home?" : "Selling handmade items, tailoring, or cooking.",
+        "Where can I find work?" : "Local markets, factories, or women’s training centers.",
+        "I don’t know how to read. Can I still get a job?" : "Yes, skills like sewing, farming, and cooking don’t need reading.",
+        "Is online money safe?" : "Yes, but don’t share your PIN with anyone."
+
     };
 
     return responses[input.toLowerCase()] || "Sorry, I don't understand that.";
